c23f491fe324a8dc9e7d79225cda8698548ff1f7
RawatPunit <rawatpunit928@gmail.com>
2023-08-14 13:36:50 +0530

    State and reducers are modified

--- movies-app/src/index.js
+++ movies-app/src/index.js
@@ -8,9 +8,18 @@ import movies from './reducers';
 
 // creating STORE
 export default function configStore(){
-   const store = createStore(movies)
+   const store = createStore(movies)        //movie from the  reducer
    return store; 
 }
+console.log('BEFORE STATE',  store.getState());     //basically will be an empty array
+
+store.dispatch({
+  tpe  : 'ADD_MOVIES',
+  movies : [{name : 'Superman'}]
+})
+console.log('AFTER STATE',  store.getState());      //gets the data with movie named SUperman
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
